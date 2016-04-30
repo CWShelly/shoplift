24c78443f2851d4205d0ce03606935f1f2dbb9ef
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-30 15:43:36 -0700

    changed crime status GET looks for from shoplifting to assault with guns

--- public/app.js
+++ public/app.js
@@ -3,10 +3,16 @@ var obj = {};
 var request = new XMLHttpRequest();
 var date = [];
 
+// https:// data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD
 
-request.open('GET', 'https://data.seattle.gov/api/views/jvdx-443h/rows.json?accessType=DOWNLOAD', true);
 
-request.open('GET', 'https://data.seattle.gov/api/views/jvdx-443h/rows.json?accessType=DOWNLOAD', true);
+// request.open('GET',
+// 'https:// data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD', true);
+
+// request.open('GET', 'https:// data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD', true);
+
+request.open('GET', 'https://data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD', true);
+// request.open('GET', 'https://data.seattle.gov/api/views/jvdx-443h/rows.json?accessType=DOWNLOAD', true);
 
 
 request.onload = function() {
@@ -14,16 +20,19 @@ request.onload = function() {
     console.log('status 200');
     var resp = request.responseText;
     var parse = JSON.parse(resp);
+    // var reparse = JSON.parse(parse);
     console.log(parse.data[0]);
     console.log(parse.data.length);
     console.log('at 12:17 pm it was 31904');
 
     for (var i = 0; i < parse.data.length; i++) {
       date.push(parse.data[i][15]);
-      obj[parse.data[i][13]] = parse.data[i][15];
+    //   console.log(parse.data[0]);
+    //   debugger;
+      obj[parse.data[i][12]] = parse.data[i][15].slice(0, 10);
       console.log(obj);
     }
-
+    console.log(parse.data[0]);
     // console.log(obj);
     // console.log(date);
   }
