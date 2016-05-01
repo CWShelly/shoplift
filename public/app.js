56c42354e0545f66ca1077a092a21d46b9d999c9
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-01 00:21:21 -0700

    changed server.js to basic http server, currently without any routes

--- public/app.js
+++ public/app.js
@@ -1,17 +1,14 @@
-
+console.log('waiting');
 var obj = {};
 var request = new XMLHttpRequest();
 var date = [];
 
-// https:// data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD
+// xv6f-9u5j
 
+request.open('GET', 'https://data.seattle.gov/api/views/jvdx-443h/rows.json?accessType=DOWNLOAD', true);
 
-// request.open('GET',
-// 'https:// data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD', true);
+// request.open('GET', 'https://data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD', true);
 
-// request.open('GET', 'https:// data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD', true);
-
-request.open('GET', 'https://data.seattle.gov/api/views/xv6f-9u5j/rows.json?accessType=DOWNLOAD', true);
 // request.open('GET', 'https://data.seattle.gov/api/views/jvdx-443h/rows.json?accessType=DOWNLOAD', true);
 
 
@@ -20,23 +17,25 @@ request.onload = function() {
     console.log('status 200');
     var resp = request.responseText;
     var parse = JSON.parse(resp);
-    // var reparse = JSON.parse(parse);
     console.log(parse.data[0]);
     console.log(parse.data.length);
-    console.log('at 12:17 pm it was 31904');
+    // console.log('at 12:17 pm it was 31904');
 
     for (var i = 0; i < parse.data.length; i++) {
-      date.push(parse.data[i][15]);
+    //   date.push(parse.data[i][15]);
+
     //   console.log(parse.data[0]);
     //   debugger;
-      obj[parse.data[i][12]] = parse.data[i][15].slice(0, 10);
-      console.log(obj);
+      obj[parse.data[i][12]] = parse.data[i][15];
+    //   console.log(obj);
+      date.push(obj);
     }
-    console.log(parse.data[0]);
-    // console.log(obj);
-    // console.log(date);
+    console.log(parse.data[0].length);
+    console.log(obj);
+    console.log(date);
   }
 };
+
 
 request.onerror = function() {
   console.log('errors');
