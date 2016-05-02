83d608676be80bd106dde80d5f684013d6065f40
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-02 00:48:18 -0700

    added superagent for ajax requests to data.seattle.gov. Still unsure how to post without using a CLI, however the data does get stored, according to schema, in a db. Need to figure out how to show more than 20 at a time.

--- models/crime.js
+++ models/crime.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 var crimeSchema = new mongoose.Schema({
   offense: { type: String },
-  date: { type: Date },
+  date: { type: String },
   longitude: { type: Number },
   lattitude: { type: Number },
   zone: { type: String }
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-// const crimeRouter = require(__dirname + '/routes/crime_router');
 const mongoose = require('mongoose');
+
 const Crime = require(__dirname + '/models/crime');
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/crimes_db');
 
-// mongoose.connect('mongodb://localhost/crimes_db');
 
 const crimeRouter = require(__dirname + '/routes/crime_router');
+
+
 
 app.use('/api', crimeRouter);
 app.listen(PORT, () => console.log('server up on ' + PORT));
